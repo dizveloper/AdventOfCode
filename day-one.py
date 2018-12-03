2ba4df1b764bfae85f585ce3e8a2f20d0109a1dd
dizdarevic <dizdarevic1994@gmail.com>
2018-12-02 20:03:19 -0500

    using itertools

--- day-one.py
+++ day-one.py
@@ -1,35 +1,13 @@
-# total = 0
+import itertools
 
-# with open("day-one-input.txt", "r") as inp:
-#     for line in inp:
-#         try:
-#             num = float(line)
-#             total += num
-#         except ValueError:
-#             print("{} is not a number!".format(line))
+data = [int(x) for x in open("day-one-input.txt").readlines()]
+print(sum(data))
 
-# print("Total of all numbers: {}".format(total))
-
-
-infile = open("day-one-input.txt", "r")
-infile.readline()  # skip the first line
-nums = []
-running_total = 0
-freqs = [0]
-first_repeated = 0
-
-# while first_repeated == 0:
-for line in infile:
-    num = line.split()
-    nums.append(int(num[0]))
-    running_total += int(num[0])
-    if running_total in freqs:
-        first_repeated = running_total
-        print(running_total)
-    freqs.append(running_total)
-
-
-print("PART 1: total", running_total)
-print("PART 2: first repeat frequency", first_repeated)
-
-infile.close()
+freq = 0
+seen = {0}
+for num in itertools.cycle(data):
+    freq += num
+    if freq in seen:
+        print(freq)
+        break
+    seen.add(freq)
