b4d852e5be419c696ed4620f6546001a176b6131
dizdarevic <dizdarevic1994@gmail.com>
2018-12-02 20:16:54 -0500

    using itertools

--- day-one.py
+++ day-one.py
@@ -1,13 +1,13 @@
 import itertools
 
-data = [int(x) for x in open("day-one-input.txt").readlines()]
-print(sum(data))
+input = [int(x) for x in open("day-one-input.txt").readlines()]
+print(sum(input))
 
-freq = 0
+first_repeated_freq = 0
 seen = {0}
-for num in itertools.cycle(data):
-    freq += num
-    if freq in seen:
-        print(freq)
+for num in itertools.cycle(input):
+    first_repeated_freq += num
+    if first_repeated_freq in seen:
+        print(first_repeated_freq)
         break
-    seen.add(freq)
+    seen.add(first_repeated_freq)
