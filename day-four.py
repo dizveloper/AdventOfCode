4e6813e6e8b12bf3944859209c0e5df389e4b7af
Edvin Dizdarevic <edvin.dizdarevic@32586-mbpr.2d.hq>
2018-12-05 13:12:25 -0500

    four

--- day-four.py
+++ day-four.py
@@ -19,158 +19,37 @@ with open("day-four-input.txt") as data:
 #     json.dump(GuardSchedule, f)
 
 AllGuards = re.findall(r"\#(.[0-9]+)", GuardIDs)
-TimeChart = [["." for x in range(120)] for y in range(len(AllGuards))]
+TimeChart = [[0 for x in range(120)] for y in range(len(AllGuards))]
 
 AsleepTimeChart = [[0 for x in range(2)] for y in range(len(AllGuards))]
+IDs = [0] * int(len(AllGuards))
 
 for i, id in enumerate(AllGuards):
     AsleepTimeChart[i][0] = int(id)
     TimeChart[i][0] = int(id)
+    IDs[i] = int(id)
 
-index = 0
+# AsleepTimeChart.reverse()
+
+# index = 0
 current_guard_index = 0
-sleep_check_index = 0
+sleep_check_index = 1
 for event in GuardSchedule:
 
     if re.search(r"\#(.[0-9]+)", event["happening"]):
         start_shift = re.search(r"\#(.[0-9]+)", event["happening"])
         current_guard = start_shift.group(1)
         sleep_check_index = 1
-        # This needs  to change to be any with current_guard
-        print(
-            TimeChart.index(
-                [
-                    int(current_guard),
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                    ".",
-                ]
-            )
-        )
-    #     current_guard_index = # index where key = current_guard
+        current_guard_index = IDs.index(int(current_guard))
 
     if re.search(r"asleep*", event["happening"]):
         TimeChart[current_guard_index][sleep_check_index] += 1
+        AsleepTimeChart[current_guard_index][1] += 1
 
-    index += 1
+    # index += 1
     sleep_check_index += 1
 
-# print(TimeChart)
 
-# fill AsleepTimeChart[1] with totals of each TimeChart[guard_id] ignoring the first
+for i, id in enumerate(AsleepTimeChart):
+    if int(AsleepTimeChart[i][1]) != 0:
+        print(AsleepTimeChart[i])
