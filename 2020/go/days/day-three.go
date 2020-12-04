1dd25505504d18a9094d7df939fc4471f88856f1
dizveloper <dizdarevic1994@gmail.com>
2020-12-03 19:45:33 -0500

    completed day 3 part 2, but need to clean up the answer

--- 2020/go/days/day-three.go
+++ 2020/go/days/day-three.go
@@ -330,28 +330,48 @@ func Three() {
 		".#...##..##.#.........#.##...#.",
 	}
 
+	slopes := [5][2]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+
+	trees := []int{}
+
+	for slope := 0; slope < len(slopes); slope++ {
+		treesAtSlope := treesInSlope(inputSlice, slopes[slope][0], slopes[slope][1])
+		trees = append(trees, treesAtSlope)
+	}
+
+	fmt.Print("Trees encountered: ")
+	fmt.Println(trees)
+}
+
+func treesInSlope(grid []string, right int, down int) int {
 	trees := 0
 	row := 0
 	column := 0
 
 	for {
-		if column >= len(inputSlice[0]) {
-			column = column % len(inputSlice[0])
+		if column >= len(grid[0]) {
+			column = column % len(grid[0])
 		}
 
-		if row == len(inputSlice) {
-			fmt.Print("Trees encountered: ")
-			fmt.Println(trees)
+		if row >= len(grid) {
 			break
 		}
 
-		if isItATree(inputSlice[row], column) {
+		if isItATree(grid[row], column) {
 			trees++
 		}
 
-		column = column + 3
-		row++
+		column = column + right
+		row = row + down
 	}
+
+	return trees
 }
 
 func isItATree(row string, columnPos int) bool {
