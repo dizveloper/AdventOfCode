4218dbe54593223f7956f92aec7bb74b2115619e
dizveloper <dizdarevic1994@gmail.com>
2020-12-14 15:51:37 -0500

    remvoed comment

--- 2020/days/day14.go
+++ 2020/days/day14.go
@@ -13,13 +13,6 @@ import (
 func Fourteen() {
 	inputSlice := inputs.Day14
 
-	// inputSlice := []string{
-	// 	"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
-	// 	"mem[8] = 11",
-	// 	"mem[7] = 101",
-	// 	"mem[8] = 0",
-	// }
-
 	twoD := [][]string{}
 
 	for _, line := range inputSlice {
