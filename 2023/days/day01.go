6d106ea3cae19bc965370cd43debbbdd8a806701
edvin-dizdarevic <edvin.dizdarevic@mailchimp.com>
2023-12-01 11:47:29 -0500

    day 1 p2 done

--- 2023/days/day01.go
+++ 2023/days/day01.go
@@ -4,6 +4,7 @@ import (
 	"aoc2022/inputs"
 	"fmt"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -35,11 +36,65 @@ func One() {
 	}
 	fmt.Println("Part1:", sumCalib)
 
-	// listOfNumbersAsWords := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	mapOfNums := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
 
-	// for _, val := range calib_vals {
+	sumCalib2 := 0
 
-	// }
+	for _, val := range calib_vals {
+		extracted := map[int]int{} // K = index found, V = value
 
-	fmt.Println("Part2:")
+		for k, v := range mapOfNums {
+			if strings.Contains(val, k) {
+				strInd := strings.Index(val, k)
+				strIndLast := strings.LastIndex(val, k)
+				extracted[strInd] = v
+				extracted[strIndLast] = v
+			}
+		}
+
+		comboStr := strconv.Itoa(extracted[minKey(extracted)]) + strconv.Itoa(extracted[maxKey(extracted)])
+		comboInt, _ := strconv.Atoi(comboStr)
+		sumCalib2 += comboInt
+	}
+
+	fmt.Println("Part2: ", sumCalib2)
+}
+
+func maxKey(numbers map[int]int) int {
+	max := -10000
+	for k := range numbers {
+		if k > max {
+			max = k
+		}
+	}
+	return max
+}
+
+func minKey(numbers map[int]int) int {
+	min := 100000000000000000
+	for k := range numbers {
+		if k < min {
+			min = k
+		}
+	}
+	return min
 }
--- 2023/inputs/day01.go
+++ 2023/inputs/day01.go
@@ -8,6 +8,16 @@ var Day1ex = []string{
 	"treb7uchet",
 }
 
+var Day1ex2 = []string{
+	"two1nine",
+	"eightwothree",
+	"abcone2threexyz",
+	"xtwone3four",
+	"4nineeightseven2",
+	"zoneight234",
+	"7pqrstsixteen",
+}
+
 var Day1 = []string{
 	"14gxqgqsqqbxfpxnbccjc33eight",
 	"eight2sevenkl",
